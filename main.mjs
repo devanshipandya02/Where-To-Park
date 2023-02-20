ee0f023aaa3f1cfcfa690807265d76c5129de3d5
reboot13-git <rautkrutik013@gmail.com>
2023-02-20 23:10:04 +0530

    update

--- main.mjs
+++ main.mjs
@@ -4,11 +4,7 @@ import express from 'express';
 import ejs from 'ejs';
 import path from 'path';
 import {fileURLToPath} from 'url';
-const Insta = require('instamojo-nodejs')
 const bodyParser = require('body-parser')
-
-const API_KEY = "test_2e409090ac66fabb5ca1cd05bf0"
-const AUTH_KEY = "test_5a8698ef8c344ca26255b9631ec"
 
 Insta.setKeys(API_KEY,AUTH_KEY)
 
@@ -43,34 +39,7 @@ app.get('/payment', (req, res) => {
     res.render('payment');
 })
 
-app.post('/pay',(req,res) => {
-    var name = req.body.name
-    var email = req.body.email
-    var amount = req.body.amount
-    
-//     var data = new Insta.PaymentData();
 
-//     const REDIRECT_URL = "http://localhost:3000/success";
-
-//     data.setRedirectUrl(REDIRECT_URL);
-//     data.send_email = "True";
-//     data.purpose="Test";
-//     data.amount = amount;
-//     data.name = name;
-//     data.email = email;
-
-
-//     Insta.createPayment(data, function(error,response) {
-//         if(error) {
-//         } else {
-            
-//             console.log(response)
-//             res.send("Please check your email to make payment")
-            
-//         }
-//     });
-
-// });
 
 app.get('/success',(req,res) => {
 
@@ -78,7 +47,7 @@ app.get('/success',(req,res) => {
     let random = Math.floor(Math.random() * 100) + 1; 
 
 
-    let success = "Your parking slot is " + alphabets[Math.floor(Math.random() * alphabets.length)] +"-" + random
+    let success = "Your parking slot is " + alphabets[Math.floor(Math.random() * alphabets.length)] +"-" + random;
     res.send(success)
     
 })
@@ -92,4 +61,3 @@ app.get('style.css', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
 });
-
